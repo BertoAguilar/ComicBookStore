da2afaab915cebbbebced8adf16b853b0e438b32
Luka <lukaneek@gmail.com>
2024-10-31 16:51:34 -0500

    search feature

--- src/main/java/com/alberto/comicbookstore/Repository/ComicRepository.java
+++ src/main/java/com/alberto/comicbookstore/Repository/ComicRepository.java
@@ -6,9 +6,14 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.alberto.comicbookstore.Models.Comic;
+import com.alberto.comicbookstore.Models.Genre;
 
 @Repository
 public interface ComicRepository extends CrudRepository<Comic, Long> {
 	// this method retrieves all the comics from the database
 	List<Comic> findAll();
+	
+	List<Comic> findComicByTitleContaining(String title);
+	
+	List<Comic> findComicByGenres(String[] genres);
 }
